cccc3c2e74a1e38e690fa6def8ab889038145dde
Oliver <ocai00@stuy.edu>
2020-06-12 00:27:36 -0400

    Added light and mesh

---
+++ objmethods.py
@@ -0,0 +1,102 @@
+from display import *
+from matrix import *
+from draw import *
+
+mesh_v = {}
+groups = {}
+
+def parse_obj(file):
+    global mesh_v
+    global groups
+    quads = []
+    triangles = []
+    g = None
+    count = 0
+    v_index = 1
+    for index in range(len(file)):
+        print(file[index])
+        if 'v ' in file[index]:
+            mesh_v[str(v_index)] = file[index].split(' ')
+            v_index += 1
+        # elif 'g' in file[index]:
+        #             if g:
+        #                 groups[g] = count
+        #                 count = 0
+        #                 g = file[index].split(' ')[1]
+        #             else:
+        #                 g = file[index].split(' ')[1]
+        elif 'f ' in file[index]:
+            
+            face = file[index].split(' ')
+            if len(face) > 4:
+                if float(face[1]) >= 0:
+                    quads.append(mesh_v[face[1]][1:]) 
+                    quads.append(mesh_v[face[2]][1:])
+                    quads.append(mesh_v[face[3]][1:]) 
+                    quads.append(mesh_v[face[4]][1:])
+                    
+                else:
+                    quads.append(mesh_v[len(mesh_v) + face[1]][1:]) 
+                    quads.append(mesh_v[len(mesh_v) + face[2]][1:])
+                    quads.append(mesh_v[len(mesh_v) + face[3]][1:]) 
+                    quads.append(mesh_v[len(mesh_v) + face[4]][1:])
+
+            else:
+                if float(face[1]) >= 0:
+                    triangles.append(mesh_v[face[1]][1:]) 
+                    triangles.append(mesh_v[face[2]][1:])
+                    triangles.append(mesh_v[face[3]][1:])
+                    
+                else:
+                    triangles.append(mesh_v[len(mesh_v) + face[1]][1:]) 
+                    triangles.append(mesh_v[len(mesh_v) + face[2]][1:])
+                    triangles.append(mesh_v[len(mesh_v) + face[3]][1:])
+                                   
+            count += 1
+
+    return (quads,triangles)
+
+def draw_mesh(quads,triangles,screen,zbuffer,color):
+    if quads:
+        
+        for index in range(int(len(quads)/4)):
+            temp_index = index * 4
+            draw_line(int(float(quads[temp_index][0])),int(float(quads[temp_index][1])),
+                      int(float(quads[temp_index][2])),
+                      int(float(quads[temp_index + 1][0])),int(float(quads[temp_index + 1][1])),
+                      int(float(quads[temp_index + 1][2])),
+                      screen, zbuffer,color)
+            draw_line(int(float(quads[temp_index + 1][0])),int(float(quads[temp_index + 1][1])),
+                      int(float(quads[temp_index + 1][2])),
+                      int(float(quads[temp_index + 2][0])),int(float(quads[temp_index + 2][1])),
+                      int(float(quads[temp_index + 2][2])),
+                      screen, zbuffer,color)
+            draw_line(int(float(quads[temp_index + 2][0])),int(float(quads[temp_index + 2][1])),
+                      int(float(quads[temp_index + 2][2])),
+                      int(float(quads[temp_index + 3][0])),int(float(quads[temp_index + 3][1])),
+                      int(float(quads[temp_index + 3][2])),
+                      screen, zbuffer,color)
+            draw_line(int(float(quads[temp_index + 3][0])),int(float(quads[temp_index + 3][1])),
+                      int(float(quads[temp_index + 3][2])),
+                      int(float(quads[temp_index][0])),int(float(quads[temp_index][1])),
+                      int(float(quads[temp_index][2])),
+                      screen, zbuffer,color)
+    if triangles:
+        for index in range(int(len(triangles)/3)):
+            temp_index = index * 3
+            draw_line(int(float(quads[temp_index][0])),int(float(quads[temp_index][1])),
+                      int(float(quads[temp_index][2])),
+                      int(float(quads[temp_index + 1][0])),int(float(quads[temp_index + 1][1])),
+                      int(float(quads[temp_index + 1][2])),
+                      screen, zbuffer,color)
+            draw_line(int(float(quads[temp_index + 1][0])),int(float(quads[temp_index + 1][1])),
+                      int(float(quads[temp_index + 1][2])),
+                      int(float(quads[temp_index + 2][0])),int(float(quads[temp_index + 2][1])),
+                      int(float(quads[temp_index + 2][2])),
+                      screen, zbuffer,color)
+            draw_line(int(float(quads[temp_index + 2][0])),int(float(quads[temp_index + 2][1])),
+                      int(float(quads[temp_index + 2][2])),
+                      int(float(quads[temp_index][0])),int(float(quads[temp_index][1])),
+                      int(float(quads[temp_index][2])),
+                      screen, zbuffer,color)
+
